2d2e0d0a08322dcd24e5a695c7eec9fbcf4c246d
SriginSasidharan <srigin.sasidharan@tsworkz.com>
2024-02-10 13:52:54 +0530

    feat:project created

---
+++ Artista/src/main/java/com/example/artista/dtos/BaseClassDto.java
@@ -0,0 +1,5 @@
+package com.example.artista.dtos;
+
+public class BaseClassDto {
+    private String id;
+}
---
+++ Artista/src/main/java/com/example/artista/dtos/ProductDto.java
@@ -0,0 +1,12 @@
+package com.example.artista.dtos;
+
+public class ProductDto extends BaseClassDto {
+    private String title;
+    private String category;
+    private double price;
+    private String image;
+    private String description;
+
+
+
+}
