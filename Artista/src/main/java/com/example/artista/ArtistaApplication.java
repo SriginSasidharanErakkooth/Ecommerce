66b66dc96d6918fb82332f308182b804a8d077ee
SriginSasidharan <srigin.sasidharan@tsworkz.com>
2024-02-10 12:54:23 +0530

    feat:project created

---
+++ Artista/src/main/java/com/example/artista/ArtistaApplication.java
@@ -0,0 +1,13 @@
+package com.example.artista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistaApplication.class, args);
+	}
+
+}
---
+++ Artista/src/test/java/com/example/artista/ArtistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
